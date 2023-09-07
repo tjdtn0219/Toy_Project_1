504c67e67e25d7518c33b6e9497545fa7c9feef6
rmaWhrdl <wwscan3@gmail.com>
2023-09-07 18:05:26 +0900

    Refactor : View 예외처리 및 1차 리펙토링
    - ItineraryView, SelectOptionView, TripView 예외처리
    - ConsoleView에 있던 chooseFileType() 메서드를 ItemView로 이동시키고, ItineraryView와 TripView가 구현
    - ViewErrorCode에 에러코드 추가
    - view 클래스 전체적으로 불필요한 코드 제거 및 자동 코드 포맷팅

--- src/main/java/org/exception/ViewErrorCode.java
+++ src/main/java/org/exception/ViewErrorCode.java
@@ -6,9 +6,15 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ViewErrorCode {
-    NO_MENU_OPTION("해당하는 메뉴 번호가 없습니다. 1-5사이로 다시 입력 하세요."),
+    NO_MENU_OPTION("\n[ERROR] 해당하는 메뉴 번호가 없습니다. 1-5사이의 숫자를 다시 입력해주세요.\n"),
 
-    NOT_MATCHED_DATE_FORMAT("날짜 포맷이 잘못 입력되었습니다. yyyy-mm-dd로 입력 하세요")
+    NOT_MATCHED_DATE_FORMAT("\n[ERROR] 날짜 포맷이 잘못 입력되었습니다. 날짜를 YYYY-MM-DD 형식에 맞춰 다시 입력해주세요.\n"),
+
+    NOT_MATCHED_TIME_FORMAT("\n[ERROR] 날짜, 시간 포맷이 잘못 입력되었습니다. 날짜와 시간을 YYYY-MM-DD hh:mm 형식에 맞춰 다시 입력해주세요.\n"),
+
+    NOT_MATCHED_INTEGER_FORMAT("\n[ERROR] ID는 숫자만 입력이 가능합니다. 다시 입력해주세요.\n"),
+
+    NOT_MATCHED_FILETYPE("\n[ERROR] 파일 타입은 JSON과 CSV만 가능합니다. 다시 입력해주세요.\n"),
     ;
 
     private final String message;
