4dd060d82878cd863821cb6baff24e377a7f8a08
parkgeonwoo <pgw111111@naver.com>
2023-09-07 10:17:08 +0900

    Refactor:Model에서 인터페이스를 통한 객체 의존성 추가

--- src/main/java/org/frontcontroller/controller/Impl/TripListController.java
+++ src/main/java/org/frontcontroller/controller/Impl/TripListController.java
@@ -1,11 +1,13 @@
 package org.frontcontroller.controller.Impl;
 
 import org.dto.TripDTO;
+import org.frontcontroller.controller.Controller;
 import org.model.Impl.ItineraryModelImpl;
 import org.model.Impl.TripModelImpl;
 import org.dto.ResponseItineraryDTO;
 import org.dto.ResponseTripDTO;
-import org.frontcontroller.Controller;
+import org.model.ItineraryModel;
+import org.model.TripModel;
 import org.view.TripView;
 
 import java.util.ArrayList;
@@ -13,19 +15,21 @@ import java.util.List;
 
 public class TripListController implements Controller {
 
-    private TripModelImpl tripModelImpl = TripModelImpl.getInstance();
-    private ItineraryModelImpl itineraryModelImpl = ItineraryModelImpl.getInstance();
+    private TripModel tripModel = new TripModelImpl();
+    private ItineraryModel itineraryModel = new ItineraryModelImpl();
 
     @Override
     public void process() {
 //        System.out.println("여행 조회");
         TripView tripView = new TripView();
 
-        List<ResponseTripDTO> responseTripDTOS = tripModelImpl.findAll();    //이 부분을 Model에서 만들어주세요.
+         // 폴더에 있는 모든 여행리스트를 가져옴
+        List<ResponseTripDTO> responseTripDTOS = tripModel.findAll();
+        System.out.println(responseTripDTOS.toString());
 
-        List<TripDTO.Response> responseList = new ArrayList<>();
-        for (ResponseTripDTO responseTripDTO : responseTripDTOS) {
-            List<ResponseItineraryDTO> itineraries = itineraryModelImpl.findAllByTripId(responseTripDTO.getId());    //이 부분을 Model에서 구현
+          List<TripDTO.Response> responseList = new ArrayList<>();
+          for (ResponseTripDTO responseTripDTO : responseTripDTOS) {
+            List<ResponseItineraryDTO> itineraries = itineraryModel.findAllByTripId(responseTripDTO.getDirPath());
 
             TripDTO.Response response = TripDTO.Response.fromEntity(responseTripDTO, itineraries);
             responseList.add(response);
